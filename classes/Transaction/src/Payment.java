a427b440b8c7a8386a00e07448c1b6c2b0206cc9
Stefan Stoian <Stoian-1@student.tudelft.nl>
2024-02-23 14:54:32 +0100

    Added the Payment Class with an empty class for tests as well

---
+++ classes/Transaction/src/Payment.java
@@ -0,0 +1,104 @@
+import java.util.Objects;
+
+public class Payment extends Transaction {
+    private String sender;
+    private String recipient;
+
+    /**
+     * Constructor for the payment class
+     * @param owner the owner of the transaction
+     * @param date the date of the transaction
+     * @param amount the amount of the transaction
+     * @param sender the sender of the payment
+     * @param recipient the recipient of the payment
+     */
+    public Payment(String owner, String date, float amount, String sender, String recipient) {
+        super(owner, date, amount);
+        this.sender = sender;
+        this.recipient = recipient;
+    }
+
+    /**
+     * The owner of the transaction is also the sender of the payment
+     * @param owner the owner of the transaction
+     * @param date the date of the transaction
+     * @param amount the amount of the transaction
+     * @param recipient the recipient of the payment
+     */
+    public Payment(String owner, String date, float amount, String recipient) {
+        super(owner, date, amount);
+        this.sender = owner;
+        this.recipient = recipient;
+    }
+
+    /**
+     * Getter for the sender
+     * @return the sender
+     */
+    public String getSender() {
+        return sender;
+    }
+
+    /**
+     * Setter for the sender
+     * @param sender the new sender
+     */
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    /**
+     * Getter for the recipient
+     * @return the recipient
+     */
+    public String getRecipient() {
+        return recipient;
+    }
+
+    /**
+     * Setter for the recipient
+     * @param recipient the new recipient
+     */
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    /**
+     * Generates a string representation of the payment
+     * @return the string
+     */
+    @Override
+    public String toString() {
+        return "Payment{" + super.toString() +
+            "sender='" + sender + '\'' +
+            ", recipient='" + recipient + '\'' +
+            '}';
+    }
+
+    /**
+     * The equals method
+     * @param object the object to be compared to
+     * @return true iff the two objects are equal
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if(object == null) return false;
+        if (!object.getClass().equals(getClass())) return false;
+        if (!super.equals(object)) return false;
+
+        Payment that = (Payment) object;
+
+        return Objects.equals(sender, that.sender) &&
+            Objects.equals(recipient, that.recipient);
+    }
+
+    /**
+     * Generates a hashcode
+     * @return the hashcode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), sender, recipient);
+    }
+}

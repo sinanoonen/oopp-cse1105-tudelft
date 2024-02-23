1fba3d5450e4e944a6fcab659bfaaa7fdebc680c
Sinan Onen <S.Onen@student.tudelft.nl>
2024-02-23 20:16:45 +0100

    Checkstyle Adjustments of Expense and Payment Class

--- commons/src/main/java/transactions/Expense.java
+++ commons/src/main/java/transactions/Expense.java
@@ -2,16 +2,23 @@ package transactions;
 
 import java.text.DecimalFormat;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
-public class Expense extends Transaction{
+/**
+ * Expense class that extends from the transaction class.
+ */
+public class Expense extends Transaction {
     // Title of the expense
     private String description;
     // participants mapped to how much money they owe
     Map<String, Float> participants;
 
     /**
-     * Constructor method
+     * Constructor method.
+     *
      * @param owner who paid the expense
      * @param date when was the expense paid
      * @param amount how much was paid
@@ -22,13 +29,17 @@ public class Expense extends Transaction{
         super(owner, date, amount);
         this.description = description;
         this.participants = new HashMap<>();
-        if(participants == null)
+        if (participants == null) {
             return;
-        participants.forEach(participant -> {this.participants.put(participant, 0f);});
+        }
+        participants.forEach(participant -> {
+            this.participants.put(participant, 0f);
+        });
     }
 
     /**
-     * Getter for description
+     * Getter for description.
+     *
      * @return description
      */
     public String getDescription() {
@@ -36,7 +47,8 @@ public class Expense extends Transaction{
     }
 
     /**
-     * Getter for description
+     * Getter for description.
+     *
      * @return description
      */
     public Map<String, Float> getParticipants() {
@@ -44,7 +56,8 @@ public class Expense extends Transaction{
     }
 
     /**
-     * Setter for description
+     * Setter for description.
+     *
      * @param description new description
      */
     public void setDescription(String description) {
@@ -52,14 +65,16 @@ public class Expense extends Transaction{
     }
 
     /**
-     * Updates the debt of a participant for this expense
+     * Updates the debt of a participant for this expense.
+     *
      * @param participant participant whose debt should be changed
      * @param change change in debt (+ => more debt)
      * @return true if operation performed successfully, false otherwise
      */
     public boolean modifyParticipant(String participant, float change) {
-        if(!participants.containsKey(participant))
+        if (!participants.containsKey(participant)) {
             return false;
+        }
         // This assumes that values > 0 represent debt
         float newValue = participants.get(participant) + change;
         participants.put(participant, newValue);
@@ -67,59 +82,75 @@ public class Expense extends Transaction{
     }
 
     /**
-     * Removes participant from expense,
-     * and splits their debt evenly among others
+     * Removes participant from expense
+     * and splits their debt evenly among others.
+     *
      * @param participant participant to be removed
      * @return true if operation performed successfully, false otherwise
      */
     public boolean removeParticipant(String participant) {
-        if(!participants.containsKey(participant))
+        if (!participants.containsKey(participant)) {
             return false;
+        }
         float amountToSplit = participants.remove(participant);
         float splitAmount = amountToSplit / participants.size();
         // round to 2 dp
         splitAmount = Float.parseFloat(new DecimalFormat("#.##").format(splitAmount));
         float remainder = amountToSplit - (participants.size() * splitAmount);
-        for(String p : participants.keySet()) {
+        for (String p : participants.keySet()) {
             // If there is a remainder of n cents, the first n people pay 1 cent extra
             float change = splitAmount + remainder > 0 ? 0.01f : 0;
             remainder -= 0.01f;
-            if(!modifyParticipant(p, change)) // check successful modification
+            if (!modifyParticipant(p, change)) { // check successful modification
                 return false;
+            }
         }
         return true;
     }
 
     /**
-     * toString of the expense method
+     * toString of the expense method.
+     *
      * @return string representing the expense object
      */
     @Override
     public java.lang.String toString() {
-        return "Expense{" + super.toString() +
-                "description='" + description + '\'' +
-                ", participants=" + participants +
-                '}';
+        return "Expense{" + super.toString()
+                + "description='"
+                + description + '\''
+                + ", participants="
+                + participants
+                + '}';
     }
 
     /**
-     * Equals method of the expense
+     * Equals method of the expense.
+     *
      * @param object object to compare to
      * @return true or false
      */
     public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        if (!super.equals(object)) {
+            return false;
+        }
 
         Expense expense = (Expense) object;
 
-        if (!Objects.equals(description, expense.description)) return false;
+        if (!Objects.equals(description, expense.description)) {
+            return false;
+        }
         return Objects.equals(participants, expense.participants);
     }
 
     /**
-     * Hashcode for the expense
+     * Hashcode for the expense.
+     *
      * @return integer hash of the expense object
      */
     public int hashCode() {
--- commons/src/main/java/transactions/Payment.java
+++ commons/src/main/java/transactions/Payment.java
@@ -1,7 +1,6 @@
 package transactions;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Objects;
 
 /**

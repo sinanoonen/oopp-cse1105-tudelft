2041d974176f6c4465ff8eb3fffcda45661d930c
Stefan Stoian <Stoian-1@student.tudelft.nl>
2024-02-25 17:16:19 +0100

    Added the Tag Class

---
+++ commons/src/test/java/TagTest.java
@@ -0,0 +1,11 @@
+//import java.awt.Color;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+/**
+ * For testing the Tag class.
+ */
+public class TagTest {
+
+}

9accd311acbbeb1704a11df72ab7a39262c153b5
Filip Keerberg <filip.keerberg@gmail.com>
2024-02-25 18:26:28 +0100

    fixed checkstyle

--- commons/src/main/java/Event.java
+++ commons/src/main/java/Event.java
@@ -1,5 +1,8 @@
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import org.apache.commons.lang3.RandomStringUtils;
 import transactions.Transaction;
 

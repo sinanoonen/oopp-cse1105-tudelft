e461a2271b3127c29622e8a108ae912a119fdd2e
Emilio Salvatore Gennaro Costanza <e.s.gennarocostanza@student.tudelft.nl>
2024-03-01 11:53:08 +0100

    bugfixes

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -10,7 +10,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import java.awt.Color;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * The Event class.
@@ -169,6 +176,9 @@ public class Event {
      * @return true if operation successful, false otherwise
      */
     public boolean addParticipant(User user) {
+        if (user == null) {
+            return false;
+        }
         return participants.add(user);
     }
 

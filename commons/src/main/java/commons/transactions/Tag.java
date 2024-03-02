f20ad8b1bc67d8f343dab333b3ed9a1898b7b81d
Filip Keerberg <filip.keerberg@gmail.com>
2024-03-02 14:21:35 +0100

    added ID to tags and made db memory based

--- commons/src/main/java/commons/transactions/Tag.java
+++ commons/src/main/java/commons/transactions/Tag.java
@@ -1,6 +1,8 @@
 package commons.transactions;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.awt.Color;
 import java.util.Objects;
@@ -11,6 +13,8 @@ import java.util.Objects;
 @Entity
 public class Tag {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private String name;
     private Color color;
 

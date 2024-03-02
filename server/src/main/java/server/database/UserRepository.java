af7c8a5308af1e46820a34b305167e916eaaf202
Filip Keerberg <filip.keerberg@gmail.com>
2024-03-02 21:13:13 +0100

    Checkstyle 😭

--- server/src/main/java/server/database/UserRepository.java
+++ server/src/main/java/server/database/UserRepository.java
@@ -2,9 +2,8 @@ package server.database;
 
 import commons.User;
 import commons.UserKey;
-import java.util.List;
-
 import jakarta.transaction.Transactional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;

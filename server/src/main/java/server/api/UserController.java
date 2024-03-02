46d8b55b720ed24fb71ec8110c09275e54b224e1
Filip Keerberg <filip.keerberg@gmail.com>
2024-03-02 15:18:14 +0100

    Added basic getter endpoint and a post procedure

---
+++ server/src/main/java/server/api/UserController.java
@@ -0,0 +1,62 @@
+package server.api;
+
+import commons.User;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.database.UserRepository;
+
+/**
+ * A controller to access the users of the database.
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository repo;
+
+    public UserController(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    /**
+     * Endpoint to get a user by their email.
+     *
+     * @param email email to query by
+     * @return found user
+     */
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        List<User> results = repo.getUserByEmail(email);
+        if (results == null || results.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(results.getFirst());
+    }
+
+    /**
+     * Endpoint to add a new user to the database.
+     *
+     * @param user user to be added
+     * @return added user
+     */
+    @PostMapping(path = {"", "/"})
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        if (user == null
+            || isNullOrEmpty(user.getName())
+            || isNullOrEmpty(user.getEmail())
+            || isNullOrEmpty(user.getIban())
+            || isNullOrEmpty(user.getBic())
+        ) {
+            System.out.println("Received bad post request");
+            return ResponseEntity.badRequest().build();
+        }
+        System.out.println("Received valid post request:");
+        System.out.println(user);
+        User saved = repo.save(user);
+        return ResponseEntity.ok(saved);
+    }
+
+    private boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+}
--- server/src/main/java/server/database/UserRepository.java
+++ server/src/main/java/server/database/UserRepository.java
@@ -2,9 +2,15 @@ package server.database;
 
 import commons.User;
 import commons.UserKey;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 /**
  * Repository for users.
  */
-public interface UserRepository extends JpaRepository<User, UserKey> {}
+public interface UserRepository extends JpaRepository<User, UserKey> {
+    @Query(value = "SELECT * FROM USERS u WHERE u.EMAIL=?1", nativeQuery = true)
+    List<User> getUserByEmail(String email);
+}

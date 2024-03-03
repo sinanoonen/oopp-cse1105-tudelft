eb0005c6227b101a3b999b6cbad71380f926816e
Stefan Stoian <Stoian-1@student.tudelft.nl>
2024-03-03 01:09:10 +0100

    Started working on the Event Controller

---
+++ server/src/main/java/server/api/EventController.java
@@ -0,0 +1,81 @@
+package server.api;
+
+import commons.Event;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.database.EventRepository;
+
+/**
+ * A controller for events.
+ */
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+    private final EventRepository repo;
+
+    public EventController(EventRepository repo) {
+        this.repo = repo;
+    }
+
+    /**
+     * Get all events.
+     *
+     * @return all events
+     */
+    @GetMapping(path = { "", "/" })
+    public List<Event> getAll() {
+        return repo.findAll();
+    }
+
+    /**
+     * Get an event by its UUID.
+     *
+     * @param uuid the UUID of the event
+     * @return the event
+     */
+    @GetMapping("/{uuid}")
+    public ResponseEntity<Event> getById(@PathVariable("uuid") UUID uuid) {
+        if (!repo.existsById(uuid)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.findById(uuid).get());
+    }
+
+    /**
+     * Add an event.
+     *
+     * @param event the event to add
+     * @return the added event
+     */
+    @PostMapping(path = { "", "/" })
+    public ResponseEntity<Event> add(@RequestBody Event event) {
+
+        if (event.getInviteCode() == null || isNullOrEmpty(event.getTitle())
+            || event.getTags() == null || event.getParticipants() == null
+            || event.getTransactions() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Event saved = repo.save(event);
+        return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * Checks if a string is null or empty.
+     *
+     * @param s the string s
+     * @return true if the string is null or empty
+     */
+    private static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
+}
---
+++ server/src/test/java/server/api/EventControllerTest.java
@@ -0,0 +1,79 @@
+package server.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import commons.Event;
+import commons.User;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Tests for Event Controller.
+ */
+public class EventControllerTest {
+
+    private TestEventRepository repo;
+    private EventController sut;
+
+    /**
+     * A test event repository.
+     */
+    @BeforeEach
+    public void setup() {
+        repo = new TestEventRepository();
+        sut = new EventController(repo);
+    }
+
+    @Test
+    public void cannotAddNullEvent() {
+        var actual = sut.add(getEvent(null));
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+    }
+
+    @Test
+    public void databaseIsUsed() {
+        sut.add(getEvent("test"));
+        repo.calledMethods.contains("save");
+    }
+
+    private static Event getEvent(String title) {
+        return new Event(title, new User("testName", "testMail", "testIBAN", "testBic"));
+    }
+
+    @Test
+    public void addValidEvent() {
+        Event event = getEvent("New Event");
+        ResponseEntity<Event> response = sut.add(event);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(repo.calledMethods.contains("save"));
+        assertTrue(repo.events.contains(event));
+    }
+
+    @Test
+    public void getAllEvents() {
+        List<Event> list = new ArrayList<>();
+
+        Event event1 = getEvent("Event1");
+        Event event2 = getEvent("Event2");
+
+        list.add(event1);
+        list.add(event2);
+
+        sut.add(event1);
+        sut.add(event2);
+
+        List<Event> response = sut.getAll();
+
+        assertEquals(response, list);
+        assertTrue(repo.calledMethods.contains("findAll"));
+    }
+
+
+}

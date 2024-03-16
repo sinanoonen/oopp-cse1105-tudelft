48ce1a7c3a053c976f7f32498ba7e4ba0479bf42
Stefan Stoian <Stoian-1@student.tudelft.nl>
2024-03-16 21:04:57 +0100

    Added the option to sort by title and the option to see the details of an event by double clicking it

--- client/src/main/java/client/scenes/AdminOverviewCtrl.java
+++ client/src/main/java/client/scenes/AdminOverviewCtrl.java
@@ -3,15 +3,25 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import commons.Event;
+import java.util.Comparator;
 import java.util.List;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+
 
 /**
  * The controller for the admin overview.
@@ -98,13 +108,63 @@ public class AdminOverviewCtrl {
             protected void updateItem(Event event, boolean empty) {
                 super.updateItem(event, empty);
                 setText(empty || event == null ? null : event.getTitle());
+                setOnMouseClicked(mouseEvent -> {
+                    if (mouseEvent.getClickCount() == 2 && !empty) {
+                        showEventDetailsPopup(event);
+                    }
+                });
             }
         });
     }
 
+    private void showEventDetailsPopup(Event event) {
+        Dialog<String> dialog = new Dialog<>();
+        dialog.setTitle("Event Details");
+
+        DialogPane dialogPane = dialog.getDialogPane();
+        dialogPane.setStyle("-fx-background-color: #333333; -fx-text-fill: #FFFFFF;");
+
+        TextArea textArea = new TextArea(formatEventDetails(event));
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+        textArea.setStyle("-fx-control-inner-background: #333333; "
+            + "-fx-text-fill: #FFFFFF; "
+            + "-fx-font-size: 12pt; "
+            + "-fx-background-color: #333333; "
+            + "-fx-border-color: #c30052; ");
+
+        ScrollPane scrollPane = new ScrollPane(textArea);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setStyle("-fx-background-color: #333333;");
+
+        dialogPane.setContent(scrollPane);
+
+        ButtonType closeButton = new ButtonType("Close", ButtonBar.ButtonData.CANCEL_CLOSE);
+        dialogPane.getButtonTypes().add(closeButton);
+
+        Button closeButtonControl = (Button) dialogPane.lookupButton(closeButton);
+        closeButtonControl.setStyle("-fx-background-color: #3f3f3f; "
+            + "-fx-text-fill: #FFFFFF; -fx-border-color: #c30052; "
+            + "-fx-border-radius: 5; -fx-border-width: 2;");
+
+        dialog.showAndWait();
+    }
+
+    // to be improved
+    private String formatEventDetails(Event event) {
+        return "Title: " + event.getTitle() + "\n"
+            + "Invite Code: " + event.getInviteCode() + "\n"
+            + "Participants: " + event.getParticipants() + "\n"
+            + "Tags: " + event.getTags() + "\n"
+            + "Expenses: " + event.getExpenses() + "\n"
+            + "Payments: " + event.getPayments() + "\n";
+    }
+
     @FXML
     private void handleSortByTitle() {
-
+        ObservableList<Event> events = eventContainer.getItems();
+        events.sort(Comparator.comparing(Event::getTitle));
+        eventContainer.setItems(events);
     }
 
     @FXML

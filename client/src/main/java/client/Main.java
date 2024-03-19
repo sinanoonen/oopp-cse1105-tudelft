173bd64adad1f218a53d2a559944dea6dbf9a0cb
Sinan Onen <S.Onen@student.tudelft.nl>
2024-03-19 02:23:56 +0100

    Added color to the fxml file

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -42,7 +42,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-        launch(args);
+        launch();
     }
 
     @Override

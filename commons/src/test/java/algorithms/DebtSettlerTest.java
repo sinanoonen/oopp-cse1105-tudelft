c33b993aebd81cc0869a0341c5b0dab359f9cc30
Filip Keerberg <f.o.b.keerberg@student.tudelft.nl>
2024-03-24 20:09:02 +0100

    changed test to account for new UserKey

--- commons/src/test/java/algorithms/DebtSettlerTest.java
+++ commons/src/test/java/algorithms/DebtSettlerTest.java
@@ -7,12 +7,8 @@ import commons.User;
 import commons.transactions.Expense;
 import commons.transactions.Payment;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -29,10 +25,10 @@ public class DebtSettlerTest {
     @BeforeEach
     public void setupForTests() {
         Set<User> users = new HashSet<>();
-        User dave = new User("Dave", "dave@gmail.com", "NL123456789", "bic1");
-        User eva = new User("Eva", "eva@gmail.com", "NL987654321", "bic2");
-        User mark = new User("Mark", "mark@gmail.com", "NL111122221", "bic3");
-        User anne = new User("Anne", "anne@gmail.com", "NL444444444", "bic4");
+        User dave = new User("Dave", "dave@gmail.com", "NL123456789", "bic1", UUID.randomUUID());
+        User eva = new User("Eva", "eva@gmail.com", "NL987654321", "bic2", UUID.randomUUID());
+        User mark = new User("Mark", "mark@gmail.com", "NL111122221", "bic3", UUID.randomUUID());
+        User anne = new User("Anne", "anne@gmail.com", "NL444444444", "bic4", UUID.randomUUID());
         users.add(dave);
         users.add(eva);
         users.add(mark);

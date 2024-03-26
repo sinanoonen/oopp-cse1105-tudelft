f210fa0be8d1a261b643610bedd4b1b3a092b69e
Stefan Stoian <Stoian-1@student.tudelft.nl>
2024-03-26 19:41:52 +0100

    Created class for web socket messages + tests for it

---
+++ commons/src/main/java/commons/WebSocketMessage.java
@@ -0,0 +1,33 @@
+package commons;
+
+import java.util.Objects;
+
+public class WebSocketMessage {
+    private String content;
+
+    public WebSocketMessage() {}
+
+    public WebSocketMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof WebSocketMessage that)) return false;
+        return Objects.equals(content, that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content);
+    }
+}

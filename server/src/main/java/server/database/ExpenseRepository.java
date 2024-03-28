4cf2289cb602010962b56dd262c0a0c770f2addc
Sinan Onen <S.Onen@student.tudelft.nl>
2024-03-28 21:09:58 +0100

    bug fix

--- server/src/main/java/server/database/ExpenseRepository.java
+++ server/src/main/java/server/database/ExpenseRepository.java
@@ -2,11 +2,8 @@ package server.database;
 
 
 import commons.transactions.Expense;
-import jakarta.transaction.Transactional;
-import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+
 
 
 
@@ -14,11 +11,5 @@ import org.springframework.data.jpa.repository.Query;
  * Repository for Expenses.
  */
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-
-    @Modifying
-    @Transactional
-    @Query(value = "UPDATE expenses e JOIN transactions t ON e.id = t.id SET e.description = ?1, "
-            + "t.owner = ?2, t.date = ?3, t.amount = ?4 WHERE e.id = ?5", nativeQuery = true)
-    Integer updateExpense(String description, String owner, LocalDate date, float amount, Long id);
 }
 

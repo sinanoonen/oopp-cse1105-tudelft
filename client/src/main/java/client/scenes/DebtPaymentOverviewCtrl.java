26fac29fc1447fe440a9b9d4f78172c5dc0e6fc1
Yannick Veerman <yveerman@student.tudelft.nl>
2024-04-09 11:57:48 +0200

    Fixed payments not staying after leaving event

--- client/src/main/java/client/scenes/DebtPaymentOverviewCtrl.java
+++ client/src/main/java/client/scenes/DebtPaymentOverviewCtrl.java
@@ -181,8 +181,10 @@ public class DebtPaymentOverviewCtrl implements Initializable {
         payOffDebt.requestFocus();
         double finalConvertedValue = convertedValue;
         payOffDebt.onActionProperty().set(actionEvent -> {
-            event.addTransaction(new Payment(settlementArray[0], LocalDate.now(), (float) finalConvertedValue,
-                    ClientUtils.getCurrency(), settlementArray[settlementArray.length - 1]));
+            Payment payment = new Payment(settlementArray[0], LocalDate.now(), (float) finalConvertedValue,
+                    ClientUtils.getCurrency(), settlementArray[settlementArray.length - 1]);
+            payment = serverUtils.addPayment(event.getInviteCode(), payment);
+            event.addTransaction(payment);
             mainCtrl.showDebtOverview(event);
         });
         final double buttonTopPadding = 0.01f * pane.getPrefHeight();
